0cda38701c5497ce59f84d0e967636de2a533434
Blanchot <3512173+Blanchot@users.noreply.github.com>
2018-03-30 08:15:37 +0200

    Add files via upload

---
+++ verbs4.py
@@ -0,0 +1,30 @@
+#Here is my list of 4 letter verbs for Humanity_I
+#From here: https://www.listofverbs.net/Verbs/lists/length/4
+# https://www.listofverbs.net/Verbs/that_start_with/letter/g/length/4
+
+verbs4 = ['abet','abut','ache','ahoy','ajar,','ally','amen','ante','arse','aver','avow',
+'baby','back','bail','bait','bake','bald','bale','balk','ball','balm','band','bane','bang','bank',
+'barb','bard','bare','barf','bark','barn','bash','bask','bate','bath','bawd','bawl','beam','bean',
+'bear','beat','beck','beep','bego','bell','belt','bend','best','bias','bide','biff','bile','bill',
+'bind','bird','bite','blog','blot','blow','blue','blur','body','boff','boil','bold','bomb','bond',
+'bone','bonk','boob','book','boom','boot','bore','boss','bowl','brag','bray','brew','brim','brow',
+'buck','bulk','bull','bump','bunk','bunt','boil','buoy','burn','burp','bury','busk','bush','buss',
+'bust','busy','butt','buzz',
+'cack','cage','calk','calm','call','camp','cane','care','cart','case','cash','cast','cave','cede',
+'chap','chat','chew','chin','chip','chop','chug','chum','cite','clad','clam','clap','claw','clay',
+'clip','clog','clot','cloy','club','clue','coat','coax','cock','code','cold','coif','coil','coin',
+'coal','coke','come','comb','conk','cook','cool','coop','cope','copy','cord','core','cost','cork',
+'corn','cosh','cozy','coup','crab','crap','cram','crib','crew','crit','crow','crop','cube','cuck',
+'cuff','cull','curb','cure','curl','cuss',
+'damn','dang','dare','dark','darn','dart','dash','date','daub','dawn','daze','dead','deaf','dean',
+'deal','dear','deck','deed','deem','defy','demo','dent','deny','dial','dice','dick','diet','dime',
+'dine','ding','dirk','dish','dint','diss','dive','divy','dock','dole','doll','dome','dong','doff',
+'doom','door','dope','dose','doss','down','doxx','doze','drag','drat','draw','drop','drub','drip',
+'drug','drum','duck','duel','duet','dull','dumb','duke','dude','dump','dung','dunk','dupe','dust',
+'dusk','dyke',
+'earn','ease','easy','echo','edge','edit','emit','envy','espy','etch','even','exit',
+'face','fade','fail','fake','fame','fare','farm','fart','fast','fave','fate','fawn','faze','fear',
+'feed','feck','feel','felt','fend','feud','fete','file','fill','film','find','fine','fire','firm',
+'fish','fist','flag','flaw','flay','flee','flex','flip','flog','foil','fold','fool','foot','ford',
+'foul','form','fray','free','frog','fuck','fuel','fume','fund','furl','fuse','fuss'
+]
