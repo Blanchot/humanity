46c2671831fd91cc74be2395eb8268f8186ee7f7
Blanchot <Blanchot@users.noreply.github.com>
2018-04-19 10:18:03 +0200

    basic first edit

--- test/eight_servo_test.py
+++ test/eight_servo_test.py
@@ -1,21 +1,10 @@
 # eight_servo_test
-
-"""
-================================================
-ABElectronics Servo Pi pwm controller | PWM servo controller demo
-run with: python demo_servomove.py
-================================================
-This demo shows how to set the limits of movement on a servo
-and then move between those positions
-"""
+# using ABElectronics Servo Pi pwm controller
+# based on ABElectronics PWM servo controller demo
 
 from __future__ import absolute_import, division, print_function, \
                                                     unicode_literals
 import time
-'''
-import sys # added because it I get the first import error below
-sys.path.append('usr/local/lib/python3.5/dist-packages/ServoPi')
-'''
 
 try:
     from ServoPi import Servo
@@ -30,42 +19,17 @@ except ImportError:
         raise ImportError(
             "Failed to import library from parent folder")
 
-
-# My testing mods
-
+# SETUP
 servo = Servo(0x40)
-servo.set_low_limit(1.0)
-servo.set_high_limit(2.0)
+servo.set_low_limit(0.6) #was 1.0 
+servo.set_high_limit(2.4) #was 2.0
 servo.output_enable()
 
-'''    
-def main():
-    """
-    Main program function
-    """
-    # create an instance of the servo class on I2C address 0x40
-    servo = Servo(0x40)
-
-    # set the servo minimum and maximum limits in milliseconds
-    # the limits for a servo are typically between 1ms and 2ms.
-
-    servo.set_low_limit(1.0)
-    servo.set_high_limit(2.0)
-
-    # Enable the outputs
-    servo.output_enable()
-    
-    # move the servo across its full range in increments of 10
-    while True:
-        for i in range(0, 250, 10):
-            servo.move(1, i)
-            time.sleep(1) #was 0.05
-
-        for i in range(250, 0, -10):
-            servo.move(1, i)
-            time.sleep(1)
-
-if __name__ == "__main__":
-    main()
-
+'''
+Note: all indications of left/right are from my frontal view
+Note: numeric arm ranges are from up to down
+Figure 1 (top left): left_arm: s6 (0-250), right_arm s5 (250-0)
+Figure 2 (top right): left_arm: s2 (0-250), right_arm s1 (250-0)
+Figure 3 (bot left): left_arm: s10 (0-250), right_arm s9 (250-0)
+Figure 4 (bot right): left_arm: s4 (0-250), right_arm s3 (250-0)
 '''
