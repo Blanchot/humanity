f7495743b6d5e5a93f158ea7cd85a24ab1f35a6b
Blanchot <Blanchot@users.noreply.github.com>
2020-03-05 09:25:33 +0100

    Compiled shortest list

--- shortestList.py
+++ shortestList.py
@@ -1,1 +1,23 @@
+#shortestList.py selected from master list of 4 letter verbs
 
+verbs= [
+'baby','bear','beat','best','bind','bomb','bore','bury','calm','call','chat','coax','code','cook','crit',
+'cure','damn','dare','date','defy','demo','deny','doom','draw','drug','duel','dull','dupe','echo','edit',
+'envy','espy','face','fail','fake','fart','fear','feed','feel','felt','feud','fill','film','find','fine',
+'fire','flag','flee','flog','fool','form','free','fuck','fuel','fund','gave','germ','gift','gilt','gird',
+'give','grab','grip','grok','grow','hack','halt','hang','harm','hate','have','heal','hear','heed','held',
+'help','hide','hire','hoax','hold','hump','hung','hunt','hurt','hush','hype','jail','jeer','jest','jilt',
+'join','joke','junk','keep','kept','kick','kill','king','kiss','knew','know','lack','lead','left','lend',
+'lent','lick','lift','like','list','loan','loot','lose','lost','love','lure','lust','mail','make','mask',
+'mate','meet','mend','milk','miss','mock','move','muse','mute','name','need','note','nuke','numb','obey',
+'omit','open','oust','owed','paid','pass','pick','pimp','pity','play','poke','poop','pope','pose','post',
+'pray','puke','pump','punk','push','rang','rank','rant','rape','read','rime','risk','rock','rode','rued',
+'ruin','rule','rush','sang','sank','save','scam','scar','seal','seek','sell','shat','ship','shot','show',
+'shun','sick','sign','sing','sink','sire','skip','slam','slap','slay','slew','slow','slug','snub','sold',
+'spam','spat','spin','spur','stab','stir','stop','stow','stun','suck','sued','suit','sunk','swap','take',
+'tame','tape','task','tell','tend','test','text','thou','tidy','tilt','time','tire','toil','told','tomb',
+'took','tout','trap','trip','tune','turn','twin','undo','urge','used','verb','veto','view','void','vote',
+'wage','wail','wait','wake','walk','wank','want','warn','wash','wear','wept','whip','will','wink','wish',
+'woke','womb','wore','work','xray']
+
+print('Shortest list of verbs loaded. Length: ' + str(len(verbs)))
